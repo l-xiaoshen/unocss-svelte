744738eaf03afb0da8efa000fcdc20e24cf52302
xx <157230144+l-xiaoshen@users.noreply.github.com>
2026-01-10 01:19:13 +0800

    no layers atm

--- examples/sveltekit-example/unocss.config.ts
+++ examples/sveltekit-example/unocss.config.ts
@@ -2,6 +2,5 @@ import presetWind4, { type Theme } from "@unocss/preset-wind4"
 import { defineConfig } from "unocss"
 
 export default defineConfig<Theme>({
-	outputToCssLayers: true,
 	presets: [presetWind4()],
 })
